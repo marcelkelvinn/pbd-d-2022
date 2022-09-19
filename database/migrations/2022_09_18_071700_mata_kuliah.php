ef7638738302aa2759a0a894d967e7d3dee4ca7c
Tanziil <100509407+Tanziil20@users.noreply.github.com>
2022-09-19 17:11:39 +0700

    Update Table Mata Kuliah

--- database/migrations/2022_09_18_071700_mata_kuliah.php
+++ database/migrations/2022_09_18_071700_mata_kuliah.php
@@ -13,7 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('Mata_Kuliah', function (Blueprint $table) {
+            $table->varchar('Kode_MK',10)->primary;
+            $table->smallint('SKS');
+            $table->varchar('Mata_Kuliah',45);
+            
+            $table->foreign('Mahasiswa_NIM')->references('NIM')->on('Mahasiswa');
+            $table->foreign('Dosen_NIP')->references('NIP')->on('Dosen');
+        });
+        
     }
 
     /**
@@ -23,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('Mata_Kuliah');
     }
 };
