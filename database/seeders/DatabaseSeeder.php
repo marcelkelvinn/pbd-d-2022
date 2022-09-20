4f3ba84730e8d54657360f5e264790ad04768321
myalhafiz <myusufalhafiz@gmail.com>
2022-09-20 12:31:24 +0700

    update database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -52,5 +52,45 @@ class DatabaseSeeder extends Seeder
         'Angkatan' => '2020',
         'Email' => 'CintaSalsa@student.ub.ac.id'
     ]);
+
+    DB::table('Mahasiswa')->insert([
+        'NIM' => '215150701111831',
+        'ID_Program_Studi' => '2',
+        'Nama' => 'Dinda Latifa Putri',
+        'Angkatan' => '2021',
+        'Email' => 'DindaLP@student.ub.ac.id'
+    ]);
+
+    DB::table('Mahasiswa')->insert([
+        'NIM' => '225150700111448',
+        'ID_Program_Studi' => '2',
+        'Nama' => 'Agung Wijaya',
+        'Angkatan' => '2022',
+        'Email' => 'AwiTI22@student.ub.ac.id'
+    ]);
+
+    DB::table('Mahasiswa')->insert([
+        'NIM' => '225150701111697',
+        'ID_Program_Studi' => '1',
+        'Nama' => 'Muhammad Farhan Sakha',
+        'Angkatan' => '2022',
+        'Email' => 'Sakhahama@student.ub.ac.id'
+    ]);
+
+    DB::table('Mahasiswa')->insert([
+        'NIM' => '215150707111711',
+        'ID_Program_Studi' => '3',
+        'Nama' => 'Raniya Salsabila',
+        'Angkatan' => '2021',
+        'Email' => 'Ranibila27@student.ub.ac.id'
+    ]);
+
+    DB::table('Mahasiswa')->insert([
+        'NIM' => '215150707111321',
+        'ID_Program_Studi' => '2',
+        'Nama' => 'Bagas Adi Putra',
+        'Angkatan' => '2021',
+        'Email' => 'AdiTX04@student.ub.ac.id'
+    ]);
     }
 }
