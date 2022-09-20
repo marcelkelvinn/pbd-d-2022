26e58c2be510ddb4c21968b33cd66e78ed187afd
myalhafiz <myusufalhafiz@gmail.com>
2022-09-20 12:29:25 +0700

    update database seeders.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,5 +26,31 @@ class DatabaseSeeder extends Seeder
         'ID_Program_Studi' => '1',
         'Prodi' => 'Sistem Informasi'
     ]);
+
+    DB::table('Program_Studi')->insert([
+        'ID_Program_Studi' => '2',
+        'Prodi' => 'Teknologi Informasi'
+    ]);
+
+    DB::table('Program_Studi')->insert([
+        'ID_Program_Studi' => '3',
+        'Prodi' => 'Pendidikan Teknologi Informasi'
+    ]);
+
+    DB::table('Mahasiswa')->insert([
+        'NIM' => '195150707111530',
+        'ID_Program_Studi' => '1',
+        'Nama' => 'Rihan Malik',
+        'Angkatan' => '2019',
+        'Email' => 'RihanMalik@student.ub.ac.id'
+    ]);
+
+    DB::table('Mahasiswa')->insert([
+        'NIM' => '205150700111456',
+        'ID_Program_Studi' => '3',
+        'Nama' => 'Salsabila Cinta',
+        'Angkatan' => '2020',
+        'Email' => 'CintaSalsa@student.ub.ac.id'
+    ]);
     }
 }
